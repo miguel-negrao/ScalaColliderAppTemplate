a8ca9c15d3dc7a5c4689550f9fa0a3e6e4df7386
Sciss <contact@sciss.de>
2012-03-01 01:34:58 +0000

    initialise GUI, so icon doesn't bounce forever in the Dock, and application can be properly quit

--- src/main/scala/doe/john/myscalacolliderapp/MyScalaColliderApp.scala
+++ src/main/scala/doe/john/myscalacolliderapp/MyScalaColliderApp.scala
@@ -4,9 +4,18 @@ import de.sciss.synth._
 import ugen._
 import java.io.File
 
-object MyScalaColliderApp {
+object MyScalaColliderApp extends App with Runnable {
+  java.awt.EventQueue.invokeLater(this)
 
-  def main(args: Array[String]) {
+  def run() {
+    val f = new javax.swing.JFrame("ScalaCollider")
+    val lb = new javax.swing.JLabel("Booting...")
+    f.getContentPane().add(lb, java.awt.BorderLayout.CENTER)
+    f.setSize(250,50)
+    f.setResizable(false)
+    f.setLocationRelativeTo(null)
+    f.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE)
+    f.setVisible(true)
 
     val cfg = Server.Config()
     cfg.programPath = (new File( "Contents/Resources/scsynth" ).getAbsolutePath).toString
@@ -15,18 +24,11 @@ object MyScalaColliderApp {
     // server as its argument
     println("Starting scsynth.")
     Server.run( cfg ) { s =>
-    println("scsynth started")  
-    // play is imported from package de.sciss.synth.
-    // it provides a convenience method for wrapping
-    // a synth graph function in an `Out` element
-    // and playing it back.
+      lb.setText("scsynth started")  
       play {
         val f = LFSaw.kr( 0.4 ).madd( 24, LFSaw.kr( Seq( 8, 7.23 )).madd( 3, 80 )).midicps
         CombN.ar( SinOsc.ar( f ) * 0.04, 0.2, 0.2, 4 )
       }
-      println("sound played.")
     }
-
   }
-
 }
